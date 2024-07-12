f6ac3815a5d47c1c6151ad3f6d214c37046daf8a
Cedric Royer <cedricroyerpro@gmail.com>
2024-07-12 20:13:31 +0200

    exercices done

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,15 @@
-// votre code ici
+function checkAge(age) {
 
+    switch (true) {
+        case age < 18:
+            return 'Vous êtes mineur.';
+        case age < 65:
+            return 'Vous êtes majeur.';
+            case age >= 65:
+                return 'Vous êtes senior.';
+        default:
+            return 'Erreur de donnée';
+    }
+        
+}
 export default checkAge;
